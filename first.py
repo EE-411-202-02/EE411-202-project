faaf40bf4d0c527930949c09475b91f09620cffd
Hassan <hassan97wsh@gmail.com>
2021-02-12 17:51:18 +0300

    project files added

---
+++ first.py
@@ -0,0 +1,54 @@
+import numpy as np
+import scipy.constants as con
+import time
+import adi
+import matplotlib.pyplot as plt
+
+# Time delay
+i = 0
+td = 0
+while i < 1000:
+    t0, t1 = time.thread_time_ns(), time.thread_time_ns()
+    td = max(td, t1 - t0)
+    i += 1
+
+# Setup(input):
+resolution = 50                     # Range resolution in m.
+maxRange = 5e3                     # Maximum range in m.
+fc = int(5.8e9)                     # Carrier frequency.
+fsx = 4                             # Fs = fsx*2*pulseBW. *has to be supported by the ADC
+plutoIP = "192.168.2.1"
+
+# Setup(calculations):
+rangeSteps = int(maxRange/resolution)   # Number of range steps.
+propSpeed = con.speed_of_light          # Propagation speed.
+pulseBW = propSpeed/(2*resolution)      # Pulse bandwidth.
+pulseWidth = 1/pulseBW                  # Pulse width in s.
+fs = fsx*pulseBW                        # Sampling frequency.
+prf = propSpeed/(2*maxRange)            # Repetition frequency.
+numSamples = int((1/prf)*fs+2*fsx)      # Number of samples to process.
+
+# Tx calculation:
+txS = np.zeros(20)
+txS[fsx:2*fsx] = 2**14             # samples are between -2^14 and +2^14
+
+# Tx setup:
+#sdr = adi.Pluto(plutoIP)            # Connect to SDR.
+#sdr.sample_rate = int(fs)           # send sampling rate (shared between Rx and Tx).
+#sdr.tx_rf_bandwidth = int(fs)       # same as sampling rate.
+#sdr.tx_lo = int(fc)                 # Tx local oscillator.
+#sdr.tx_hardwaregain_chan0 = 0       # 0 for lowest gain, (-90 for highest).
+
+# Tx send:
+#sdr.tx(txS)                        # Send the signal.
+#sdr.tx_cyclic_buffer = True        # Enable cyclic buffers.
+#sdr.tx_destroy_buffer()            # Must be used to send a different signal.
+
+# output
+print("Number of range steps:", rangeSteps)
+print("Buffer size:", numSamples, 'samples.')
+print("Pulse BW:", pulseBW/1000000, "MHz")
+print("Sampling frequency:", fs/1000000, "MHz")
+print("Pulse width:", pulseWidth*1e9, "ns")
+print("Time delay:", td, "ns")
+print("Steps skipped:", int(round(td/(pulseWidth*1e9))))
---
+++ testPlot.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from tkinter import *
+
+root = Tk()
+l0 = Label(root, text="Hello world!").grid(row=0, column=0)
+l0 = Label(root, text="DEEZ NUTZ!").grid(row=1, column=0)
+
+root.mainloop()
+
+
+#t = np.linspace(0, 8 * np.pi, 200)
+#s = np.cos(t)
+
+#plt.figure(1)
+#plt.plot(t, s)
+#plt.show()
